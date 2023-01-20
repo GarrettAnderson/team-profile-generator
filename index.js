a6d1bcea606d5ee3d06963748d2ce86a8be557b1
Garrett <garrettleegrahamanderson@gmail.com>
2023-01-20 16:21:35 -0500

    added super to the classes. got the questions to prompt for the manager and then the yes or no to prompt for adding a new team member

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = ({ name, location, github, linkedin }) =>
+const generateHTML = ({ name, employeeID, email, officeNumber }) =>
   `
-  
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <title>Document</title>
+    </head>
+    <body>
+    <header class="p-5 mb-4 header bg-light">
+        <div class="container">
+        <h1 class="display-4">My Team</h1>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+            ${name}
+            </div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">${employeeID}</li>
+            <li class="list-group-item">${email}</li>
+            <li class="list-group-item">${officeNumber}</li>
+            </ul>
+        </div>
+    </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
+    </html>
+
   `;
 
 const managerQuestions = [
@@ -29,6 +57,14 @@ const managerQuestions = [
     }
   ]
 
+  const addTeamMember = [
+    {
+        type: 'confirm',
+        name: 'addTeamMember',
+        message: 'Would you like to add an engineer or an intern?'
+      }
+  ]
+
   const employeeTypeQuestions = [
     {
         type: 'list',
@@ -52,12 +88,34 @@ const managerQuestions = [
       }
   ]
 
-  managerQuestions.then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+  function promptAddTeamMember() {
+    inquirer.prompt(addTeamMember)
+        .then((confirmedAnswer) => {
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
-    );
-  });
+            if (confirmedAnswer.addTeamMember === 'Yes') {
+                console.log('y was chosen')
+            }
+            console.log('n was chosen')
+        })
+  }
+
+
+  function promptQuestions() {
+    inquirer.prompt(managerQuestions)
+        .then((managerAnswers) => {
+        const htmlPageContent = generateHTML(managerAnswers);
+            
+        promptAddTeamMember()
+        
+    
+    
+    
+        // fs.writeFile('index.html', htmlPageContent, (err) =>
+        //   err ? console.log(err) : console.log('Successfully created index.html!')
+        // );
+      });
+  }
+
+  promptQuestions()
 
 
