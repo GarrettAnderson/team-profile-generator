1dfc151bb4c9540c0230aeaa11c8cb484767b1d2
Garrett <garrettleegrahamanderson@gmail.com>
2023-01-25 17:34:20 -0500

    removed the html parser package from the index.html. updated the test property in the package.json to jest

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-var HTMLParser = require('node-html-parser');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
